686b4f40b729d6fe0f0a83074901c0a1d0df0b8b
Marco Guerri <marco.guerri.dev@fastmail.com>
2023-07-02 12:06:04 +0200

    CRC working

--- main.go
+++ main.go
@@ -11,6 +11,70 @@ import (
 	"strings"
 )
 
+func crc8(b []byte) uint8 {
+    var param2 int32
+    var bVar uint8
+
+	size := len(b)
+    for param2 = 0; param2 < int32(size); param2++ {
+        bVar += uint8(b[param2])
+    }
+
+	// TODO: explain what this is euivalent to
+    return (bVar ^ 0x000000ff) + 1
+}
+
+func crc32(b []byte) uint32 {
+
+    var unaff_EBX uint32
+    unaff_EBX = 0xFFFFFFFF
+    var uVar3 uint32
+    var uVar4 uint32
+
+    for uVar4 = 0 ; uVar4 < uint32(len(b)); uVar4++ {
+       var uVar2 uint32
+       uVar2 = uint32(b[uVar4])
+
+
+       for uVar1:=0; uVar1<8; uVar1++ {
+               if (((unaff_EBX ^ uint32(uVar2)) & 1) == 0) {
+                   uVar3=0
+                } else {
+                    uVar3=0xedb88320;
+                }
+                unaff_EBX = unaff_EBX >> 1 ^ uVar3;
+                uVar2 = uVar2 >> 1;
+        }
+	}
+
+	return unaff_EBX^0xFFFFFFFF
+}
+
+
+type Range struct {
+	Start uint32
+	Length uint32
+}
+
+
+var directoryCrcRange = Range{
+	Start: 20,
+	Length: 96,
+}
+
+
+var directoryCrcLocation = Range {
+	Start: 117,
+	Length: 1,
+}
+
+
+var vpdCrcRange = Range {
+	Start: 116,
+	Length: 136,
+}
+
+
 type TypeSize uint32
 
 type Entry struct {
@@ -40,42 +104,57 @@ type Header struct {
 	Directory  Directory
 	SingleByte uint8 // define what this is
 	DirCRC     uint8
-	Vpd        [86]uint8
+	Vpd        [134]uint8
 	Crc        uint32
-
-	bytes []byte
 }
 
 func (h *Header) FromReader(r io.Reader) error {
 
-	headerSize := binary.Size(h)
-
-	b := make([]byte, headerSize)
-
-	err := binary.Read(bytes.NewBuffer(b), binary.BigEndian, headerSize)
+	err := binary.Read(r, binary.BigEndian, h)
 	if err != nil {
 		return fmt.Errorf("could not read NVRAM header: %s", err)
 	}
 
-	h.bytes = b
 	return nil
 }
 
-func (h *Header) Bytes() []byte {
-	return h.bytes
-}
 
 type NVRAM struct {
-	header Header
-	bytes  []byte
+	Header Header
+	Data  []byte
 }
 
-func (n *NVRAM) Bytes() []byte {
-	return n.bytes
+
+func (n *NVRAM) GetRange(r Range) ([]byte, error) {
+	headerSize := uint32(binary.Size(n.Header))
+	totalSize := headerSize + uint32(len(n.Data))
+
+	if r.Start + r.Length > totalSize {
+		return nil, fmt.Errorf("r.Start (%d) + r.Length (%d) = (%d) > len(NVRAM) (%d)", r.Start, r.Length, r.Start+r.Length, totalSize)
+	}
+
+	// Range starts in the header. There is a limitation that the 
+    // range must stay within the header. This could probably be 
+    // removed.
+	if r.Start <  headerSize {
+		if r.Start + r.Length > headerSize {
+			return nil, fmt.Errorf("range starts within header size and must stay within header size (%d > %d)", r.Start+r.Length, headerSize)
+		}
+    	buff := new(bytes.Buffer)
+    	if err := binary.Write(buff, binary.BigEndian, &n.Header); err != nil {
+        	return nil, fmt.Errorf("could not write header: %w", err)
+    	}
+		return buff.Bytes()[r.Start:r.Start+r.Length], nil
+	}
+
+	// Range starts in the data section. We already checked that the overall
+	// lenght is within the NVRAM range
+	return n.Data[headerSize+r.Start: headerSize+r.Start+r.Length], nil
 }
+
 
 func (n *NVRAM) FromReader(r io.Reader) error {
-	if err := n.header.FromReader(r); err != nil {
+	if err := n.Header.FromReader(r); err != nil {
 		return err
 	}
 
@@ -84,7 +163,7 @@ func (n *NVRAM) FromReader(r io.Reader) error {
 		return fmt.Errorf("could not read data: %w", err)
 	}
 
-	n.bytes = b
+	n.Data = b
 	return nil
 
 }
@@ -93,8 +172,60 @@ type directoryChecksum struct {
 }
 
 func (d directoryChecksum) check(nvram *NVRAM) error {
+
+    buff := new(bytes.Buffer)
+    if err := binary.Write(buff, binary.BigEndian, &nvram.Header); err != nil {
+        return fmt.Errorf("could not write header: %w", err)
+    }
+
+
+	directoryBytes, err := nvram.GetRange(directoryCrcRange)
+	if err != nil {
+		return fmt.Errorf("could not get directory bytes: %w", err)
+	}
+
+	oldCrcRaw, err := nvram.GetRange(directoryCrcLocation)
+	if err != nil {
+		return fmt.Errorf("could not get existing directory CRC: %w", err)
+	}
+
+	var oldCrcDir uint8 
+	if err := binary.Read(bytes.NewBuffer(oldCrcRaw), binary.BigEndian, &oldCrcDir); err != nil {
+		return fmt.Errorf("could not interpret CRC as 1 byte big endian number: %w", err)
+	}
+
+	newCrcDir := crc8(directoryBytes)
+	if oldCrcDir != newCrcDir {
+		return fmt.Errorf("existing directory crc (0x%x) mismatches calculated directory crc (0x%x)", oldCrcDir, newCrcDir)
+	}
+
 	return nil
 }
+
+
+type vpdChecksum struct {
+}
+
+
+func (v vpdChecksum) check(nvram *NVRAM) error {
+
+	vpdBytes, err := nvram.GetRange(vpdCrcRange)
+	if err != nil {
+		return fmt.Errorf("could not get vpd bytes: %w", err)
+	}
+
+	crc := crc32(vpdBytes)
+
+	crcLe := (crc & 0xFF << 24) | (crc & 0xFF00 << 8) | (crc & 0xFF0000 >> 8) | (crc & 0xFF000000 >> 24)
+
+	if nvram.Header.Crc != crcLe {
+		return fmt.Errorf("existing CRC (%x) does not match VPD crc (%x)", nvram.Header.Crc, crcLe)
+
+	}
+
+	return nil
+}
+
 
 // Check defines the interface for pre and post checks which are executed
 // on the NVRAM
@@ -133,6 +264,7 @@ func main() {
 
 	checks := []Check{
 		directoryChecksum{},
+		vpdChecksum{},
 	}
 
 	for _, check := range checks {
